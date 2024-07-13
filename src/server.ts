8d89fc5055f438120ec62dbfa301fdae7b415fbe
Josué Ocanha Costa <contato_josuecosta@hotmail.com>
2024-07-13 10:54:13 -0400

    update: criação de rota para cadastro de novo filme e configuração de menssagens de error.

--- src/server.ts
+++ src/server.ts
@@ -4,22 +4,42 @@ import { PrismaClient } from "@prisma/client";
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
+app.use(express.json());
 
 app.get("/movies", async (_, res) => {
   const movies = await prisma.movie.findMany({
     orderBy: {
       release_data: "desc",
     },
-    include:{
+    include: {
       genres: true,
       languages: true,
     },
   });
 
   res.json(movies);
-  
 });
 
+app.post("/movies", async (req, res) => {
+  try {
+    console.log(`Dados a serem cadastrados na api. ${req.body}`);
+    const { title, release_data, genre_id, language_id, oscar } = req.body;
+
+    await prisma.movie.create({
+      data: {
+        title,
+        release_data: new Date(release_data),
+        genre_id,
+        language_id,
+        oscar,
+      },
+    });
+
+    res.status(201).send("Filme cadastrado com sucesso.");
+  } catch (e) {
+    res.status(500).send({ message: "Falha ao cadastrar um filme." + e });
+  }
+});
 
 app.listen(port, () => {
   console.log("Servidor em execução");

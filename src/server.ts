8260522098080810fb464b0a3f9bb32f2b9cf6c7
Josué Ocanha Costa <contato_josuecosta@hotmail.com>
2024-07-15 20:36:44 -0400

    update: adicionada atualização de filmes

--- src/server.ts
+++ src/server.ts
@@ -28,13 +28,16 @@ app.post("/movies", async (req, res) => {
     const movieWithSameTitle = await prisma.movie.findFirst({
       where: {
         title: {
-          equals: title, mode: "insensitive"
-        }
-      }
+          equals: title,
+          mode: "insensitive",
+        },
+      },
     });
 
     if (movieWithSameTitle) {
-      res.status(409).send({message: "Já existe um filme cadastrado com este titulo."});
+      res
+        .status(409)
+        .send({ message: "Já existe um filme cadastrado com este titulo." });
       return;
     }
 
@@ -50,10 +53,47 @@ app.post("/movies", async (req, res) => {
 
     res.status(200);
     res.send("Filme cadastrado com sucesso.");
-    
   } catch (e) {
     res.status(500).send({ message: "Falha ao cadastrar um filme." + e });
   }
+});
+
+app.put("/movies/:id", async (req, res) => {
+  try {
+    // pegar o id do filme
+    const id = Number(req.params.id);
+
+    // Validar existencia do cadastro.
+    const movieExist = await prisma.movie.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!movieExist55) {
+      return res.status(404).send({ message: "Filme não encontrado!" });
+    }
+
+    //Para atualizar qualquer informação do cadastro, precisaremos
+    // aplicar o spreed para que qualquer informação que venha seja adicionada no objeto do banco.
+    const data = { ...req.body };
+    data.release_data = data.release_data
+      ? new Date(data.release_data)
+      : undefined;
+
+    //buscar os dados do id
+    await prisma.movie.update({
+      where: {
+        id,
+      },
+      data: data,
+    });
+
+  } catch (error) {
+    res.status(500).send({ message: "Falha ao atualizar registro do filme"+ error });
+  }
+
+  res.status(200).send("Filme atualizado com sucesso.");
 });
 
 app.listen(port, () => {
